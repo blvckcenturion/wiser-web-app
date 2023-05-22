f88669c1e4d7ea7a82d0bfed271e51721437e077
blvckcenturion <blvckcenturion@gmail.com>
2023-05-22 19:44:37 -0400

    updated: timeout for creating a new summarization

--- src/services/summarizationService.tsx
+++ src/services/summarizationService.tsx
@@ -31,23 +31,30 @@ class SummarizationService{
         try {
             // Form url endpoint for creating a new summarization
             const url = `${process.env.NEXT_PUBLIC_API_URL}/summarization/`;
+
             // Set headers for creating a new summarization
             const headers = {
-                // Accept json
-                'Accept': 'application/json',
-                // Set content type to json
-                'Content-Type': 'application/json',
-                // Set authorization to jwt token
-                'Authorization': `Bearer ${jwt}`
+            // Accept json
+            'Accept': 'application/json',
+            // Set content type to json
+            'Content-Type': 'application/json',
+            // Set authorization to jwt token
+            'Authorization': `Bearer ${jwt}`
             };
 
             // Set data for creating a new summarization
             const data = {
-                youtube_video_url: ytUrl
+            youtube_video_url: ytUrl
             };
 
-            // Send post request to create a new summarization
-            const response: AxiosResponse = await axios.post(url, data, { headers });
+            // Set the timeout duration in milliseconds (e.g., 60 seconds)
+            const timeoutDuration = 60000;
+
+            // Send post request to create a new summarization with timeout
+            const response: AxiosResponse = await axios.post(url, data, {
+            headers,
+            timeout: timeoutDuration
+            });
             // Return response
             return response.data;
         } catch (error) {
